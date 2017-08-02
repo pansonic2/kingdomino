c5cc9791016d4d4bf7777066e6e91fdae4133990
pansonic2 <30543241+pansonic2@users.noreply.github.com>
2017-08-02 16:23:40 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ $("#morebuttons").on("click", morebtn)
   function morebtn(){
    
   $("body").append("<br></br><form name = terrain>terrain <select><option>Wheat Field<option>Forest<option>Lake<option>Grazing Ground<option>Marsh<option>Mountain</option></form>");  
-  $("body").append("<form name = crowns>crowns <select><option>1<option>2<option>3<option>4<option>5<option>6<option>7<option>8<option>9</option></form>");
-  $("body").append("<form name = tiles>tiles <select><option>1<option>2<option>3<option>4<option>5<option>6<option>7<option>8<option>9</option></form>");
+  $("body").append("<form name = crowns>crowns <select><option>1<option>2<option>3<option>4<option>5<option>6<option>7<option>8<option>9<option>10</option></form>");
+  $("body").append("<form name = tiles>tiles <select><option>1<option>2<option>3<option>4<option>5<option>6<option>7<option>8<option>9<option>10<option>11<option>12</option></form>");
     
   }
   
